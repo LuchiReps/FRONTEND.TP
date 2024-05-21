2e6b3abe5002198e42a7e73cb4f631ee81be2e87
Your Name <you@example.com>
2024-05-21 15:41:08 -0300

    historia

--- js/historia.js
+++ js/historia.js
@@ -9,14 +9,14 @@ window.onscroll = function() {
       var parrafo = document.getElementById('parrafo');
 
       if(tema2.top < 65 && tema2.top < tema3.top){
-            parrafo.innerHTML = "TEMA 2";   
+            parrafo.innerHTML = " Baja Edad Media ";   
       }
       
       if(tema3.top < 65){
-            parrafo.innerHTML = "TEMA 3";   
+            parrafo.innerHTML = " Renacimiento ";   
       }
       
       if(tema2.top > 399){
-            parrafo.innerHTML = "TEMA 1";   
+            parrafo.innerHTML = " Alta Edad Media ";   
       }
 };
