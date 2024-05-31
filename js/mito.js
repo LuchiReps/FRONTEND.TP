e859322644387a39dc485a629891c4bbd62c1a8a
fedefenner <fedefenner@gmail.com>
2024-05-30 21:58:18 -0300

    cambio de img templo

--- js/mito.js
+++ js/mito.js
@@ -1,7 +1,5 @@
-// Seleccionar todas las imágenes con la clase 'selectable'
 const mitologia = document.querySelectorAll('.seleccionado');
 
-// Seleccionar el modal y sus elementos
 const modal = document.getElementById('image-modal');
 const modalImage = document.getElementById('modal-image');
 const modalDescription = document.getElementById('modal-description');
@@ -10,10 +8,7 @@ const closeModalBtn = document.querySelector('.close-btn');
 // Agregar un evento de clic a cada imagen
 mitologia.forEach(image => {
     image.addEventListener('click', () => {
-        // Mostrar el modal
         modal.style.display = 'flex';
-        
-        // Mostrar la imagen seleccionada y la descripción correspondiente en el modal
         modalImage.src = image.src;
         modalDescription.textContent = image.getAttribute('data-text');
     });
